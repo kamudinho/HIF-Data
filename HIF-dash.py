3ea146ebea60ff409f408256ab81b556d036d360
kamudinho <kasper@kaspermundt.dk>
2026-02-06 20:10:17 +0100

    Update HIF-dash.py

--- HIF-dash.py
+++ HIF-dash.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Jeg tester!")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Lad os se hvordan det ender!."
 )
