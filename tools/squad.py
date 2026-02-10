8ab91acf86d8c57b7491018270f2971712f50baa
kamudinho <kasper@kaspermundt.dk>
2026-02-10 23:29:51 +0100

    Update squad.py

--- tools/squad.py
+++ tools/squad.py
@@ -84,7 +84,7 @@ def vis_side(df):
             if not spillere_pos.empty:
                 # Positionslabel
                 ax.text(x_pos, y_pos - 5, f" {label} ", size=12, color="white", va='center', ha='center', fontweight='bold',
-                        bbox=dict(facecolor='#df003b', edgecolor='white', boxstyle='round,pad=0.3'))
+                        bbox=dict(facecolor='#ff4b4b', edgecolor='white', boxstyle='round,pad=0.3'))
                 
                 # Spillernavne
                 for i, (_, p) in enumerate(spillere_pos.iterrows()):
