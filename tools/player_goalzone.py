13a2e60a3cac78af8bfef22fac15c39aba592409
kamudinho <kasper@kaspermundt.dk>
2026-02-11 15:51:56 +0100

    Update player_goalzone.py

--- tools/player_goalzone.py
+++ tools/player_goalzone.py
@@ -105,7 +105,7 @@ def vis_side(df_events, df_spillere):
         color_val = count / max_count
         
         rect = Rectangle((b["x_min"], b["y_min"]), b["x_max"] - b["x_min"], b["y_max"] - b["y_min"], 
-                         edgecolor='#444444', linestyle='-', linewidth=0.3, 
+                         edgecolor='#000000', linestyle='-', linewidth=0.3, 
                          facecolor=cmap(color_val), alpha=0.7, zorder=1)
         ax.add_patch(rect)
         
