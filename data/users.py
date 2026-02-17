36be7f8df83076a8f900ab2c699ff4882a34aa15
kamudinho <kasper@kaspermundt.dk>
2026-02-17 19:33:05 +0100

    Update users.py

--- data/users.py
+++ data/users.py
@@ -1,13 +1,10 @@
 # data/users.py
 
-# Format: { "brugernavn": {"pass": "kode", "role": "rolle"} }
-USER_DB = {
-    "kasper": {"pass": "kasper", "role": "admin"},
-    "ceo":    {"pass": "ceo",    "role": "Analytiker"},
-    "mr":     {"pass": "mr",     "role": "coach"},
-    "kd":     {"pass": "kd",     "role": "coach"},
-    "cg":     {"pass": "cg",     "role": "scout"}
-}
-
 def get_users():
-    return USER_DB
+    return {
+        "kasper": {"pass": "kasper", "role": "admin"},
+        "ceo":    {"pass": "ceo",    "role": "Analytiker"},
+        "mr":     {"pass": "mr",     "role": "coach"},
+        "kd":     {"pass": "kd",     "role": "coach"},
+        "cg":     {"pass": "cg",     "role": "scout"}
+    }
