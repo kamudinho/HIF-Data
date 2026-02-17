a6de88e951b0eb3f502d9cb2662321076abb581e
kamudinho <kasper@kaspermundt.dk>
2026-02-17 19:38:43 +0100

    Update users.py

--- data/users.py
+++ data/users.py
@@ -1,10 +1,9 @@
 # data/users.py
-
 def get_users():
     return {
         "kasper": {"pass": "kasper", "role": "admin"},
-        "ceo":    {"pass": "ceo",    "role": "Analytiker"},
-        "mr":     {"pass": "mr",     "role": "coach"},
-        "kd":     {"pass": "kd",     "role": "coach"},
-        "cg":     {"pass": "cg",     "role": "scout"}
+        "ceo": {"pass": "ceo", "role": "Analytiker"},
+        "mr": {"pass": "mr", "role": "coach"},
+        "kd": {"pass": "kd", "role": "coach"},
+        "cg": {"pass": "cg", "role": "scout"}
     }
