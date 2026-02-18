beef277661e14b87b4ee3d2d0db1c34f4f6c95fc
kamudinho <kasper@kaspermundt.dk>
2026-02-18 21:27:24 +0100

    Update scout_db.py

--- tools/scout_db.py
+++ tools/scout_db.py
@@ -93,7 +93,7 @@ def vis_profil(p_data, full_df, s_df):
         fig_line.update_layout(title="Rating udvikling", height=300, yaxis=dict(range=[0.8, 6.2]), plot_bgcolor='white')
         st.plotly_chart(fig_line, use_container_width=True)
 
-   with t4:
+    with t4:
         st.markdown("### Sæsonstatistik")
         
         # Tjek om stats_df findes
