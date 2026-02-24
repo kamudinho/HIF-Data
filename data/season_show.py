065ae4c115fbcd7ed423835eddfc4cc3d07d2889
kamudinho <kasper@kaspermundt.dk>
2026-02-24 12:46:20 +0100

    Update season_show.py

--- data/season_show.py
+++ data/season_show.py
@@ -3,12 +3,12 @@ SEASONNAME = "2025/2026"
 TEAM_WYID = 7490
 
 # --- Turneringer (IDs) ---
-COMPETITION_WYID = (328, 329, 43319, 331, 1305, 330)
+COMPETITION_WYID = (328, 329, 43319, 331, 1305, 335)
 
 # --- Navne til Dashboard (Valgfrit, men gør det pænt) ---
 # Denne ordbog kan vi bruge i modstanderanalysen til at vise navne i stedet for tal
 COMP_MAP = {
-    330: "U17 Ligaen"
+    335: "Superliga"
     328: "NordicBet Liga",
     329: "2. division",
     43319: "3. division",
