1b656bfe68c3d015e63cc4dde7f6d33234ea1552
kamudinho <kasper@kaspermundt.dk>
2026-02-25 21:57:10 +0100

    Update player_shots.py

--- tools/player_shots.py
+++ tools/player_shots.py
@@ -125,8 +125,8 @@ def vis_side(df_spillere=None, hold_map=None):
         
         st.pyplot(fig)
 
-# --- DEBUG PRINT (Fjern når det virker) ---
-st.write("--- DEBUG INFO ---")
-st.write(f"Første 5 ID'er fra din CSV: {list(navne_dict.keys())[:5]}")
-st.write(f"Første 5 ID'er fra Snowflake: {df_s['PLAYER_ID_CLEAN'].unique()[:5].tolist()}")
-# ------------------------------------------
+        # --- DEBUG PRINT (Fjern når det virker) ---
+        st.write("--- DEBUG INFO ---")
+        st.write(f"Første 5 ID'er fra din CSV: {list(navne_dict.keys())[:5]}")
+        st.write(f"Første 5 ID'er fra Snowflake: {df_s['PLAYER_ID_CLEAN'].unique()[:5].tolist()}")
+        # ------------------------------------------
