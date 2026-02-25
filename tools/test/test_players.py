ac8da789977ab4f02d41154734d9ff806fa91c53
kamudinho <kasper@kaspermundt.dk>
2026-02-25 14:46:21 +0100

    Update test_players.py

--- tools/test/test_players.py
+++ tools/test/test_players.py
@@ -47,7 +47,7 @@ def vis_side():
     # --- 3. TOP BRANDING ---
     st.markdown(f"""
         <div style="background-color:{hif_rod}; padding:10px; border-radius:4px; margin-bottom:10px;">
-            <h3 style="color:white; margin:0; text-align:center; font-family:sans-serif; text-transform:uppercase; letter-spacing:1px; font-size:1.1rem;">TAKTIK & KONTRAKTER</h3>
+            <h3 style="color:white; margin:0; text-align:center; font-family:sans-serif; text-transform:uppercase; letter-spacing:1px; font-size:1.1rem;">BETINIA: SPILLERSTATISTIK</h3>
         </div>
     """, unsafe_allow_html=True)
 
