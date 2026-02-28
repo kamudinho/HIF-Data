86d3dd58c95b77605e6b15f8ee444eaddbc6cda1
kamudinho <kasper@kaspermundt.dk>
2026-02-28 12:03:30 +0100

    Update season_show.py

--- data/season_show.py
+++ data/season_show.py
@@ -3,13 +3,16 @@ SEASONNAME = "2025/2026"
 TEAM_WYID = 7490
 COMPETITION_WYID = (328,)
 
-# --- Navne til Dashboard (Valgfrit, men gør det pænt) ---
-# Denne ordbog kan vi bruge i modstanderanalysen til at vise navne i stedet for tal
+# --- Navne til Dashboard ---
 COMP_MAP = {
-    335: "Superliga",
-    328: "NordicBet Liga",
+    335: "3F Superliga",
+    328: "Betinia Ligaen",  # Opdateret navn
     329: "2. division",
     43319: "3. division",
     331: "Oddset Pokalen",
-    1305: "U19 Ligaen"
+    1305: "U19 Ligaen",
+    
+    # Opta UUIDs
+    "29actv1ohj8r10kd9hu0jnb0n": "3F Superliga",
+    "6ifaeunfdelecgticvxanikzu": "Betinia Ligaen" # Opdateret navn
 }
