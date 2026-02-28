5912e95fa6e8a013aca23cea9b5f03eb8590972f
kamudinho <kasper@kaspermundt.dk>
2026-02-28 12:20:02 +0100

    Create team_mapping.py

---
+++ data/utils/team_mapping.py
@@ -0,0 +1,57 @@
+TEAMS = {
+    # --- Betinia Ligaen (1. division) ---
+    "Hvidovre IF": {"wyid": 7490, "uuid": "8gxd9ry2580pu1b1dd5ny9ymy", "league": 328},
+    "AaB": {"wyid": 7454, "uuid": "36g6ifzjliec1jqnbtf7yesme", "league": 328},
+    "Horsens": {"wyid": 7465, "uuid": "5rz9enoyknpg8ji78za5b82p0", "league": 328},
+    "Lyngby": {"wyid": 7484, "uuid": "anga7587r1zv4ey71ge9zxol3", "league": 328},
+    "Esbjerg": {"wyid": 7451, "uuid": "2v69668s6p2c68wz2hpg018h2", "league": 328},
+    "Kolding IF": {"wyid": 7622, "uuid": "d1zdf956i09p4v02r76h9h0be", "league": 328},
+    "Hobro": {"wyid": 7510, "uuid": "4t96h936f01p4v02r76h9h0be", "league": 328},
+    "HB Køge": {"wyid": 7615, "uuid": "8cfkisxf1fkxdqtt6tx1tup48", "league": 328},
+    "Hillerød": {"wyid": 7699, "uuid": "9v69668s6p2c68wz2hpg018h2", "league": 328},
+    "Aarhus Fremad": {"wyid": 7502, "uuid": "6v69668s6p2c68wz2hpg018h2", "league": 328},
+    "B.93": {"wyid": 7470, "uuid": "7v69668s6p2c68wz2hpg018h2", "league": 328},
+    "Middelfart": {"wyid": 7578, "uuid": "3v69668s6p2c68wz2hpg018h2", "league": 328},
+
+    # --- 3F Superliga ---
+    "Copenhagen": {"wyid": 7452, "uuid": "5rz9enoyknpg8ji78za5b82p1", "league": 335},
+    "Midtjylland": {"wyid": 7455, "uuid": "5rz9enoyknpg8ji78za5b82p2", "league": 335},
+    "Brøndby": {"wyid": 7453, "uuid": "5rz9enoyknpg8ji78za5b82p3", "league": 335},
+    "AGF": {"wyid": 7457, "uuid": "5rz9enoyknpg8ji78za5b82p4", "league": 335},
+    "Nordsjælland": {"wyid": 7458, "uuid": "5rz9enoyknpg8ji78za5b82p5", "league": 335},
+    "Viborg": {"wyid": 7456, "uuid": "5rz9enoyknpg8ji78za5b82p6", "league": 335},
+    "SønderjyskE": {"wyid": 7499, "uuid": "5rz9enoyknpg8ji78za5b82p7", "league": 335},
+    "OB": {"wyid": 7460, "uuid": "5rz9enoyknpg8ji78za5b82p8", "league": 335},
+    "Randers": {"wyid": 7462, "uuid": "5rz9enoyknpg8ji78za5b82p9", "league": 335},
+    "Fredericia": {"wyid": 7469, "uuid": "5rz9enoyknpg8ji78za5b82p0", "league": 335},
+    "Silkeborg": {"wyid": 7461, "uuid": "5rz9enoyknpg8ji78za5b82p1", "league": 335},
+    "Vejle": {"wyid": 7473, "uuid": "5rz9enoyknpg8ji78za5b82p2", "league": 335},
+
+    # --- 2. division ---
+    "AB": {"wyid": 7464, "league": 329},
+    "Næstved": {"wyid": 7475, "league": 329},
+    "Roskilde": {"wyid": 7497, "league": 329},
+    "Thisted": {"wyid": 7513, "league": 329},
+    "HIK": {"wyid": 7476, "league": 329},
+    "Vendsyssel": {"wyid": 7488, "league": 329},
+    "VSK Aarhus": {"wyid": 38132, "league": 329},
+    "Fremad Amager": {"wyid": 7471, "league": 329},
+    "Brabrand": {"wyid": 7482, "league": 329},
+    "Ishøj": {"wyid": 70771, "league": 329},
+    "Skive": {"wyid": 7491, "league": 329},
+    "Helsingør": {"wyid": 7566, "league": 329},
+
+    # --- 3. division ---
+    "Nykøbing": {"wyid": 7526, "league": 43319},
+    "FA 2000": {"wyid": 7644, "league": 43319},
+    "Holbæk B&I": {"wyid": 30574, "league": 43319},
+    "Brønshøj": {"wyid": 7472, "league": 43319},
+    "Hørsholm-Usserød": {"wyid": 71244, "league": 43319},
+    "Frem": {"wyid": 7463, "league": 43319},
+    "Vanløse": {"wyid": 7551, "league": 43319},
+    "Næsby": {"wyid": 7503, "league": 43319},
+    "Vejgaard": {"wyid": 25748, "league": 43319},
+    "Lyseng": {"wyid": 7649, "league": 43319},
+    "Sundby": {"wyid": 34555, "league": 43319},
+    "Odder": {"wyid": 7588, "league": 43319}
+}
