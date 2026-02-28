50cc16cc45e0c43d0116c8f2d49dfd20a0cd5721
kamudinho <kasper@kaspermundt.dk>
2026-02-28 11:44:38 +0100

    Create mapping.py

---
+++ data/utils/mapping.py
@@ -0,0 +1,22 @@
+# data/utils/mappings.py
+
+  OPTA_EVENT_TYPES = {
+    # Numeriske hændelser
+    "1": "Pass", "2": "Offside Pass", "3": "Take On", "4": "Free kick",
+    "5": "Out", "6": "Corner", "7": "Tackle", "8": "Interception",
+    "9": "Turnover", "10": "Save", "11": "Claim", "12": "Clearance",
+    "13": "Miss", "14": "Post", "15": "Attempt Saved", "16": "Goal",
+    "17": "Card", "18": "Player off", "19": "Player on", "30": "End",
+    "32": "Start", "41": "Punch", "44": "Aerial", "45": "Challenge",
+    "49": "Ball recovery", "50": "Dispossessed", "51": "Error",
+    "52": "Keeper pick-up", "55": "Offside provoked", "58": "Shot faced",
+    "59": "Keeper Sweeper", "60": "Chance Missed", "61": "Ball touch",
+    "73": "Other Ball Contact", "74": "Blocked Pass", "82": "Control",
+    "83": "Attempted tackle",
+
+    # Tekst-baserede koder (Cards, Subs, Goals)
+    "AS": "Assist", "G": "Goal", "OG": "Own Goal", "RC": "Red card",
+    "YC": "Yellow card", "Y2C": "Yellow 2nd/RC", "SI": "Substitute in",
+    "SO": "Substitute out", "PG": "Penalty goal", "PM": "Penalty missed",
+    "PS": "Penalty save", "VAR": "Video Assistant Referee"
+}
