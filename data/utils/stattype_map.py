a48283df2ae90d6e7e7c682ff658ab61cda4e6de
kamudinho <kasper@kaspermundt.dk>
2026-03-01 11:11:41 +0100

    Create stattype_map.py

---
+++ data/utils/stattype_map.py
@@ -0,0 +1,41 @@
+# data/utils/stattype_map.py
+
+STAT_TYPE_MAP = {
+    # Offensiv
+    "goals": "Mål",
+    "goalAssist": "Assists",
+    "totalScoringAtt": "Afslutninger",
+    "ontargetScoringAtt": "Skud på mål",
+    "shotOffTarget": "Skud forbi mål",
+    "blockedScoringAtt": "Blokerede afslutninger",
+    "expectedGoals": "xG",
+    "penaltyWon": "Straffespark fremprovokeret",
+    
+    # Afleveringer & Spilopbygning
+    "totalPass": "Afleveringer",
+    "accuratePass": "Succesfulde afleveringer",
+    "possessionPercentage": "Besiddelse %",
+    "totalThrows": "Indkast",
+    "goalKicks": "Målspark",
+    "cornerTaken": "Hjørnespark taget",
+    "wonCorners": "Hjørnespark vundet",
+    "lostCorners": "Hjørnespark tabt",
+    
+    # Defensiv
+    "totalTackle": "Tacklinger",
+    "wonTackle": "Tacklinger vundet",
+    "totalClearance": "Clearinger",
+    "outfielderBlock": "Blokeringer (markspiller)",
+    "totalOffside": "Offside",
+    "fkFoulWon": "Frispark vundet",
+    "fkFoulLost": "Frispark begået",
+    
+    # Målmand & Disciplin
+    "saves": "Redninger",
+    "goalsConceded": "Mål lukket ind",
+    "penaltySave": "Straffespark reddet",
+    "penaltyFaced": "Straffespark imod",
+    "penaltyConceded": "Straffespark begået",
+    "totalYellowCard": "Gule kort",
+    "subsMade": "Udskiftninger foretaget"
+}
