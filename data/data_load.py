48938c1a51f87630b6ae8108c851fb1567f63213
kamudinho <kasper@kaspermundt.dk>
2026-03-02 14:35:30 +0100

    Update data_load.py

--- data/data_load.py
+++ data/data_load.py
@@ -189,11 +189,12 @@ def get_data_package():
         "playerstats": df_playerstats,
         "player_career": df_player_career,
         "team_stats_full": df_team_stats,
-        "opta_matches": df_matches_final,
+        "opta_matches": df_matches_final,  # <--- Den vigtige flettede data
         "opta_raw_stats": df_opta_stats,     
         "logo_map": logo_map,
         "comp_filter": comp_filter,        
-        "season_filter": season_filter,  
+        "season_filter": wyscout_season_filter,  # <--- RETTET HER
+        "opta_season_uuid": opta_season_uuid,    # <--- TILFØJ EVT. DENNE
         "COMPETITION_WYID": COMPETITION_WYID,
         "SEASONNAME": SEASONNAME,
         "TEAM_WYID": TEAM_WYID,
