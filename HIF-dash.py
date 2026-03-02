ecfba17602d9827d9ce60df6d2e76ccdb05312ba
kamudinho <kasper@kaspermundt.dk>
2026-03-02 23:46:55 +0100

    Update HIF-dash.py

--- HIF-dash.py
+++ HIF-dash.py
@@ -92,9 +92,6 @@ with st.sidebar:
     if hoved_omraade == "TRUPPEN":
         sel = option_menu(None, options=["Oversigt", "Forecast"], 
                          styles={"nav-link-selected": {"background-color": "#cc0000"}})
-    elif hoved_omraade == "HIF ANALYSE":
-        sel = option_menu(None, options=["Afslutninger", "Modstanderanalyse", "Scatterplots"], 
-                         styles={"nav-link-selected": {"background-color": "#cc0000"}})
     elif hoved_omraade == "BETINIA LIGAEN":
         sel = option_menu(None, options=["Kampe"], 
                          styles={"nav-link-selected": {"background-color": "#cc0000"}})
