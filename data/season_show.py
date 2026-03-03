0d13b2d5a0850154b0bfa7c77ddcecaf4037e6a0
kamudinho <kasper@kaspermundt.dk>
2026-03-03 10:57:09 +0100

    Update season_show.py

--- data/season_show.py
+++ data/season_show.py
@@ -1,41 +1,33 @@
 from data.utils.team_mapping import COMPETITIONS
 
-# --- 1. VALG AF LIGA ---
+# --- 1. DETTE ER DET ENESTE STED DU RETTER FREMOVER ---
 VALGT_LIGA = "1. Division" 
-SEASONNAME = "2025/2026" 
-TEAM_WYID = 7490
 TOURNAMENTCALENDAR_NAME = "2025/2026" 
+TEAM_WYID = 7490
+TEAM_OPTA_UUID = "8gxd9ry2580pu1b1dd5ny9ymy"
 
-# --- 2. HENT INFO FRA CENTRAL MAPPING ---
-# Nu bruger vi de nye kolonnenavne du lige har lavet
+# --- 2. AUTOMATISK OPSÆTNING (Læser fra TEAM_MAPPING) ---
 liga_info = COMPETITIONS.get(VALGT_LIGA, {})
 COMPETITION_WYID = (liga_info.get("wyid"),)
 OPTA_COMP_UUID = liga_info.get("COMPETITION_OPTAUUID")
 
-# --- 3. AUTOMATISK COMP_MAP GENERERING ---
-COMP_MAP = {info["wyid"]: navn for navn, info in COMPETITIONS.items() if info.get("wyid")}
-# Tilføj Opta UUIDs til map hvis de findes
-for navn, info in COMPETITIONS.items():
-    if info.get("COMPETITION_OPTAUUID"):
-        COMP_MAP[info["COMPETITION_OPTAUUID"]] = navn
-
-# --- 4. HJÆLPEFUNKTIONER & BAGUDKOMPATIBEL COMP_MAP ---
+# --- 3. HJÆLPEFUNKTIONER ---
 def get_league_ids(league_name=VALGT_LIGA):
     conf = COMPETITIONS.get(league_name, {})
     ids = []
     if conf.get("wyid"): ids.append(str(conf["wyid"]))
-    if conf.get("opta_uuid"): ids.append(conf["opta_uuid"])
+    if conf.get("COMPETITION_OPTAUUID"): ids.append(conf["COMPETITION_OPTAUUID"])
     return ids
 
 def get_competition_name(id_val):
     id_str = str(id_val)
-    for name, ids in COMPETITIONS.items():
-        if id_str == str(ids["wyid"]) or id_str == ids["opta_uuid"]:
+    for name, info in COMPETITIONS.items():
+        if id_str == str(info.get("wyid")) or id_str == info.get("COMPETITION_OPTAUUID"):
             return name
     return "Ukendt Turnering"
 
-# Genererer COMP_MAP automatisk
+# Genererer COMP_MAP automatisk til brug i andre visninger
 COMP_MAP = {}
-for name, ids in COMPETITIONS.items():
-    if ids["wyid"]: COMP_MAP[ids["wyid"]] = name
-    if ids["opta_uuid"]: COMP_MAP[ids["opta_uuid"]] = name
+for name, info in COMPETITIONS.items():
+    if info.get("wyid"): COMP_MAP[info["wyid"]] = name
+    if info.get("COMPETITION_OPTAUUID"): COMP_MAP[info["COMPETITION_OPTAUUID"]] = name
