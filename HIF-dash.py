9313b60486bd6a9aaeca92b1f414c5a874dae5ff
kamudinho <kasper@kaspermundt.dk>
2026-03-03 11:23:04 +0100

    Update HIF-dash.py

--- HIF-dash.py
+++ HIF-dash.py
@@ -150,9 +150,9 @@ try:
     elif hoved_omraade == "ADMIN":
         if sel == "Rå Data Explorer":
             st.title("🛰️ Rå Data Explorer")
-            st.write("### Opta Matches", dp["opta_matches"].head(50))
-            st.write("### Opta Stats", dp["opta_raw_stats"].head(50))
-            st.write("### Tjek af alle kolonner", load_snowflake_query("opta_player_stats", None, None).head(10))
+            # Ret nøglerne her så de matcher din return-struktur
+            st.write("### Opta Matches", dp["opta"]["matches"].head(50))
+            st.write("### Opta Stats", dp["opta"]["team_stats"].head(50))
         elif sel == "Brugerstyring":
             import tools.admin as adm
             adm.vis_side()
