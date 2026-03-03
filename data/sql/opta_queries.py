d5d5fc693a255fe1819608def1c3f1af7a2bee6a
kamudinho <kasper@kaspermundt.dk>
2026-03-03 10:01:17 +0100

    Update opta_queries.py

--- data/sql/opta_queries.py
+++ data/sql/opta_queries.py
@@ -1,29 +1,28 @@
-#data/sql/opta_queries.py
+# data/sql/opta_queries.py
 
-def get_opta_queries(liga_navn, saeson_navn):
+def get_opta_queries(liga_uuid, saeson_navn): # Vi ændrer input fra navn til uuid
     DB = "KLUB_HVIDOVREIF.AXIS"
     
     return {
         "opta_matches": f"""
             SELECT *
             FROM {DB}.OPTA_MATCHINFO
+            WHERE COMPETITION_OPTAUUID = '{liga_uuid}'
+            AND TOURNAMENTCALENDAR_NAME = '{saeson_navn}'
         """,
         
         "opta_player_stats": f"""
-            SELECT 
-                PLAYER_OPTAUUID, -- VIGTIG: Sørg for at denne kolonne er stavet præcis sådan
-                PLAYER_NAME,
-                GOALS,
-                SHOTS,
-                ASSISTS,
-                PASSES,
-                SUCCESSFULPASSES,
-                MINUTESONFIELD
+            SELECT *
             FROM {DB}.OPTA_EVENTS
-            WHERE TOURNAMENT_NAME = '{liga_navn}'
+            WHERE COMPETITION_OPTAUUID = '{liga_uuid}'
+            AND TOURNAMENTCALENDAR_NAME = '{saeson_navn}'
         """,
         
         "opta_team_stats": f"""
             SELECT * FROM {DB}.OPTA_MATCHSTATS
+            WHERE MATCHID IN (
+                SELECT MATCHID FROM {DB}.OPTA_MATCHINFO 
+                WHERE COMPETITION_OPTAUUID = '{liga_uuid}'
+            )
         """
     }
