e21e379d705ec1a0f29ae10d4e014fbe6296e927
kamudinho <kasper@kaspermundt.dk>
2026-03-03 11:46:00 +0100

    Update opta_queries.py

--- data/sql/opta_queries.py
+++ data/sql/opta_queries.py
@@ -1,13 +1,18 @@
-#data/sql/opta_queries.py
+from data.utils.team_mapping import COMPETITION_NAME, TOURNAMENTCALENDAR_NAME
 
-def get_opta_queries(liga_uuid, saeson_navn):
+def get_opta_queries(liga_uuid=None, saeson_navn=None):
     DB = "KLUB_HVIDOVREIF.AXIS"
     
+    # Hvis vi ikke sender noget med, bruger vi de globale standardværdier
+    liga = liga_uuid if liga_uuid else COMPETITION_NAME
+    saeson = saeson_navn if saeson_navn else TOURNAMENTCALENDAR_NAME
+    
     return {
-        # Vi henter alt fra tabellerne uden at spørge om liga eller år
-        "opta_matches": f"SELECT * FROM {DB}.OPTA_MATCHINFO",
-        
-        "opta_player_stats": f"SELECT * FROM {DB}.OPTA_EVENTS",
-        
+        "opta_matches": f"""
+            SELECT * FROM {DB}.OPTA_MATCHINFO 
+            WHERE COMPETITION_NAME = '{liga}' 
+            AND TOURNAMENTCALENDAR_NAME = '{saeson}'
+        """,
+        "opta_player_stats": f"SELECT * FROM {DB}.OPTA_EVENTS", # Overvej filter her senere
         "opta_team_stats": f"SELECT * FROM {DB}.OPTA_MATCHSTATS"
     }
