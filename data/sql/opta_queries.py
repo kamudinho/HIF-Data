146761edbbd785d6d6b1194839f68141eb97ea78
kamudinho <kasper@kaspermundt.dk>
2026-03-04 14:11:20 +0100

    Update opta_queries.py

--- data/sql/opta_queries.py
+++ data/sql/opta_queries.py
@@ -1,47 +1,4 @@
-from data.utils.team_mapping import COMPETITION_NAME, TOURNAMENTCALENDAR_NAME
-
-def get_opta_queries(liga_uuid=None, saeson_navn=None):
-    """
-    Returnerer alle nødvendige SQL-queries til Opta-integrationen.
-    Bruger værdier fra team_mapping som default (Sæson 2025/2026).
-    """
-    DB = "KLUB_HVIDOVREIF.AXIS"
-    
-    # Prioritér input-parametre, ellers brug globale værdier fra team_mapping
-    liga = liga_uuid if liga_uuid else COMPETITION_NAME
-    saeson = saeson_navn if saeson_navn else TOURNAMENTCALENDAR_NAME
-    
-    return {
-        # 1. MATCHINFO - Definerer universet for kampene
-        "opta_matches": f"""
-            SELECT  
-                MATCH_OPTAUUID, MATCH_DATE_FULL, MATCH_STATUS,  
-                TOTAL_HOME_SCORE, TOTAL_AWAY_SCORE, WINNER,
-                MATCH_LOCALTIME, CONTESTANTHOME_OPTAUUID,  
-                CONTESTANTAWAY_OPTAUUID, CONTESTANTHOME_NAME,  
-                CONTESTANTAWAY_NAME, COMPETITION_NAME,  
-                TOURNAMENTCALENDAR_NAME, TOURNAMENTCALENDAR_OPTAUUID
-            FROM {DB}.OPTA_MATCHINFO  
-            WHERE COMPETITION_NAME = '{liga}'  
-            AND TOURNAMENTCALENDAR_NAME = '{saeson}'
-            ORDER BY MATCH_DATE_FULL DESC
-        """,
-        
-        # 2. MATCHSTATS - Henter hold-statistikker (boldbesiddelse osv.)
-        "opta_team_stats": f"""
-            SELECT  
-                MATCH_OPTAUUID, CONTESTANT_OPTAUUID, STAT_TYPE, STAT_TOTAL
-            FROM {DB}.OPTA_MATCHSTATS
-            WHERE TOURNAMENTCALENDAR_OPTAUUID IN (
-                SELECT DISTINCT TOURNAMENTCALENDAR_OPTAUUID  
-                FROM {DB}.OPTA_MATCHINFO  
-                WHERE TOURNAMENTCALENDAR_NAME = '{saeson}'
-            )
-        """,
-        
-        # 3. SHOT & CHANCE EVENTS - Henter skud, mål, assists og key passes
-        "opta_shotevents": f"""
-            SELECT  
+SELECT  
                 e.MATCH_OPTAUUID, 
                 e.EVENT_OPTAUUID, 
                 e.EVENT_CONTESTANT_OPTAUUID,
@@ -55,12 +12,12 @@ def get_opta_queries(liga_uuid=None, saeson_navn=None):
                 MAX(CASE WHEN q.QUALIFIER_QID = 140 THEN q.QUALIFIER_VALUE END) as PASS_END_X,
                 MAX(CASE WHEN q.QUALIFIER_QID = 141 THEN q.QUALIFIER_VALUE END) as PASS_END_Y,
                 LISTAGG(q.QUALIFIER_QID, ',') WITHIN GROUP (ORDER BY q.QUALIFIER_QID) as QUALIFIERS
-            FROM {DB}.OPTA_EVENTS e
-            LEFT JOIN {DB}.OPTA_QUALIFIERS q ON e.EVENT_OPTAUUID = q.EVENT_OPTAUUID
+            FROM OPTA_EVENTS e
+            LEFT JOIN OPTA_QUALIFIERS q ON e.EVENT_OPTAUUID = q.EVENT_OPTAUUID
             WHERE e.EVENT_TYPEID IN (1, 13, 14, 15, 16)
             AND e.TOURNAMENTCALENDAR_OPTAUUID IN (
-                SELECT DISTINCT TOURNAMENTCALENDAR_OPTAUUID FROM {DB}.OPTA_MATCHINFO  
-                WHERE TOURNAMENTCALENDAR_NAME = '{saeson}'
+                SELECT DISTINCT TOURNAMENTCALENDAR_OPTAUUID FROM OPTA_MATCHINFO  
+                WHERE TOURNAMENTCALENDAR_NAME = '2025/2026'
             )
             GROUP BY 
                 e.MATCH_OPTAUUID, 
@@ -73,5 +30,3 @@ def get_opta_queries(liga_uuid=None, saeson_navn=None):
                 e.EVENT_TYPEID, 
                 e.EVENT_PERIODID, 
                 e.EVENT_TIMEMIN
-        """
-    }
