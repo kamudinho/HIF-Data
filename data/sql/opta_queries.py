86bf56f9936efb12e5bc28aea47d228e9aa6da0d
kamudinho <kasper@kaspermundt.dk>
2026-03-09 22:02:24 +0100

    Update opta_queries.py

--- data/sql/opta_queries.py
+++ data/sql/opta_queries.py
@@ -68,7 +68,7 @@ def get_opta_queries(liga_f, saeson_f, hif_only=False):
             GROUP BY 1, 2
             ORDER BY LB_TOTAL DESC
             LIMIT 100
-        """
+        """,
         
         "opta_team_linebreaks": f"""
             SELECT * FROM {DB}.OPTA_TEAMLINEBREAKINGPASSAGGREGATES 
