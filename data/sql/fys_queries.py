b16fcf9e8f3a03b4ff9cde86ad34765d729d591f
kamudinho <kasper@kaspermundt.dk>
2026-03-15 22:22:56 +0100

    Update fys_queries.py

--- data/sql/fys_queries.py
+++ data/sql/fys_queries.py
@@ -1,9 +1,18 @@
 # data/sql/fys_queries.py
 
 def get_match_physical_stats(match_id):
-    query = f"""
-    SELECT *
+    # Vi bruger match_id (Opta UUID) til at filtrere
+    return f"""
+    SELECT 
+        PLAYER_NAME,
+        TEAM_NAME,
+        TEAM_WYID,
+        TOTAL_DISTANCE,
+        LOW_INTENSITY_DISTANCE,
+        MEDIUM_INTENSITY_DISTANCE,
+        HIGH_INTENSITY_DISTANCE,
+        SPRINT_DISTANCE,
+        MAX_SPEED
     FROM KLUB_HVIDOVREIF.AXIS.SECONDSPECTRUM_PLAYER_STATS
     WHERE MATCH_OPTAID = '{match_id}'
     """
-    return query
