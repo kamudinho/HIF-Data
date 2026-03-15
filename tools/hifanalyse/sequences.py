0707ae04bf97cfa3162e804cadd6c19a0b2d1e82
kamudinho <kasper@kaspermundt.dk>
2026-03-15 07:54:19 +0100

    Update sequences.py

--- tools/hifanalyse/sequences.py
+++ tools/hifanalyse/sequences.py
@@ -56,7 +56,7 @@ def vis_side(dp):
     col_main, col_side = st.columns([2.5, 1])
 
     with col_side:
-        st.subheader("Vælg Scoring")
+        st.caption("Vælg scoring")
         selected_label = st.selectbox("", options=goal_events['LABEL'].unique(), label_visibility="collapsed")
         
         selected_id = goal_events[goal_events['LABEL'] == selected_label]['SEQUENCEID'].iloc[0]
