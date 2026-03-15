6d54acf15c9eda814a53e6018f0a096112e49e9d
kamudinho <kasper@kaspermundt.dk>
2026-03-15 21:48:36 +0100

    Update fys_queries.py

--- data/sql/fys_queries.py
+++ data/sql/fys_queries.py
@@ -1,35 +1,17 @@
+# data/sql/fys_queries.py
+
 def get_match_physical_stats(match_id):
-    """
-    Henter de overordnede fysiske metrics for alle spillere i en specifik kamp.
-    """
+    # Vi bruger de officielle kolonnenavne fra AXIS/Second Spectrum
     query = f"""
     SELECT 
         PLAYER_NAME,
         TEAM_NAME,
+        TEAM_WYID, -- Meget vigtig for dit filter!
         TOTAL_DISTANCE,
         HIGH_INTENSITY_DISTANCE,
         SPRINT_DISTANCE,
-        MAX_SPEED,
-        ACCELERATIONS_HIGH,
-        DECELERATIONS_HIGH
+        MAX_SPEED
     FROM KLUB_HVIDOVREIF.AXIS.SECONDSPECTRUM_PLAYER_STATS
     WHERE MATCH_OPTAID = '{match_id}'
-    ORDER BY TOTAL_DISTANCE DESC
-    """
-    return query
-
-def get_team_physical_summary(match_id):
-    """
-    Henter totaler for holdet for at sammenligne med modstanderen.
-    """
-    query = f"""
-    SELECT 
-        TEAM_NAME,
-        SUM(TOTAL_DISTANCE) as TEAM_TOTAL_DIST,
-        SUM(SPRINT_DISTANCE) as TEAM_TOTAL_SPRINT,
-        AVG(MAX_SPEED) as TEAM_AVG_MAX_SPEED
-    FROM KLUB_HVIDOVREIF.AXIS.SECONDSPECTRUM_PLAYER_STATS
-    WHERE MATCH_OPTAID = '{match_id}'
-    GROUP BY TEAM_NAME
     """
     return query
