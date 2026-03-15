75e1401a5b7e5e729429fcd3f86e4747856982a2
kamudinho <kasper@kaspermundt.dk>
2026-03-15 22:37:01 +0100

    Update fys_queries.py

--- data/sql/fys_queries.py
+++ data/sql/fys_queries.py
@@ -1,18 +1,28 @@
 # data/sql/fys_queries.py
 
 def get_match_physical_stats(match_id):
-    # Vi bruger match_id (Opta UUID) til at filtrere
+    # Denne henter tallene pr. spiller (PLAYER_ID og PLAYER_NAME)
     return f"""
     SELECT 
         PLAYER_NAME,
         TEAM_NAME,
-        TEAM_WYID,
-        TOTAL_DISTANCE,
-        LOW_INTENSITY_DISTANCE,
-        MEDIUM_INTENSITY_DISTANCE,
-        HIGH_INTENSITY_DISTANCE,
-        SPRINT_DISTANCE,
-        MAX_SPEED
-    FROM KLUB_HVIDOVREIF.AXIS.SECONDSPECTRUM_PLAYER_STATS
-    WHERE MATCH_OPTAID = '{match_id}'
+        TEAM_ID,
+        DISTANCE_TOTAL AS TOTAL_DISTANCE,
+        DISTANCE_HI_INTENSITY AS HI_DIST,
+        DISTANCE_SPRINT AS SPRINT_DIST,
+        SPEED_MAX AS MAX_SPEED
+    FROM KLUB_HVIDOVREIF.AXIS.SECONDSPECTRUM_F53A_GAME_PLAYER
+    WHERE MATCH_ID = '{match_id}'
+    """
+
+def get_team_physical_stats(match_id):
+    # Denne henter tallene for hele holdet (din TEAM tabel)
+    return f"""
+    SELECT 
+        TEAM_NAME,
+        DISTANCE_TOTAL,
+        DISTANCE_HI_INTENSITY,
+        DISTANCE_SPRINT
+    FROM KLUB_HVIDOVREIF.AXIS.SECONDSPECTRUM_F53A_GAME_TEAM
+    WHERE MATCH_ID = '{match_id}'
     """
