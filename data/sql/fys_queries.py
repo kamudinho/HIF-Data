f1722c3cd0c511c3aae91dd56cf54834c199bf4e
kamudinho <kasper@kaspermundt.dk>
2026-03-15 21:56:51 +0100

    Update fys_queries.py

--- data/sql/fys_queries.py
+++ data/sql/fys_queries.py
@@ -1,16 +1,8 @@
 # data/sql/fys_queries.py
 
 def get_match_physical_stats(match_id):
-    # Vi bruger de officielle kolonnenavne fra AXIS/Second Spectrum
     query = f"""
-    SELECT 
-        PLAYER_NAME,
-        TEAM_NAME,
-        TEAM_WYID, -- Meget vigtig for dit filter!
-        TOTAL_DISTANCE,
-        HIGH_INTENSITY_DISTANCE,
-        SPRINT_DISTANCE,
-        MAX_SPEED
+    SELECT *
     FROM KLUB_HVIDOVREIF.AXIS.SECONDSPECTRUM_PLAYER_STATS
     WHERE MATCH_OPTAID = '{match_id}'
     """
