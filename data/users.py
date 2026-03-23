9ca0ca91ec76bf468af740f87aa5b2dcb7fba6cc
kamudinho <kasper@kaspermundt.dk>
2026-03-23 16:14:41 +0100

    Update users.py

--- data/users.py
+++ data/users.py
@@ -2,28 +2,28 @@
 def get_users():
     return {
         "kasper": {
-            "pass": "kasper", 
+            "pass": "kasper1234", 
             "role": "admin",
             "restricted": []  # Admin har ingen restriktioner - ser alt
         },
         "ceo": {
-            "pass": "ceo", 
+            "pass": "ceo1234", 
             "role": "Analytiker",
             "restricted": ["ADMIN"] # Kan se alt undtagen ADMIN-panelet
         },
         "mr": {
-            "pass": "mr", 
+            "pass": "Retov2650", 
             "role": "coach",
-            "restricted": ["ADMIN", "SCOUTING"] # Låst for Admin og Scouting
+            "restricted": ["ADMIN"] # Låst for Admin og Scouting
         },
         "kd": {
-            "pass": "kd", 
+            "pass": "Daugaard2650", 
             "role": "coach",
-            "restricted": ["ADMIN", "SCOUTING"] # Låst for Admin og Scouting
+            "restricted": ["ADMIN"] # Låst for Admin og Scouting
         },
         "cg": {
-            "pass": "cg", 
+            "pass": "Gron1234", 
             "role": "scout",
-            "restricted": ["ADMIN", "ANALYSE"] # Låst for Admin og Analyse
+            "restricted": ["ADMIN"] # Låst for Admin og Analyse
         }
     }
