27be51e2c140df7ed988c2f65162d9c3bf9d89fa
kamudinho <kasper@kaspermundt.dk>
2026-03-24 10:47:27 +0100

    Update modstanderanalyse.py

--- tools/ligaen/modstanderanalyse.py
+++ tools/ligaen/modstanderanalyse.py
@@ -138,6 +138,7 @@ def vis_side(analysis_package=None):
                 fig, ax = pitch.draw(figsize=(4, 6))
                 if not df_f.empty:
                     sns.kdeplot(x=df_f['LOCATIONY'], y=df_f['LOCATIONX'], fill=True, cmap='Reds', alpha=0.6, ax=ax, bw_adjust=0.8)
+                ax.set_ylim(0, 55)
                 draw_logo_on_ax(ax, t_logo)
                 st.pyplot(fig); plt.close(fig)
             with c2:
