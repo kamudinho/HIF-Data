0598172208dc5a0461cc26b7b3508129a5cbd26c
kamudinho <kasper@kaspermundt.dk>
2026-03-31 00:07:43 +0200

    Update emne_db.py

--- tools/scouting/emne_db.py
+++ tools/scouting/emne_db.py
@@ -84,10 +84,10 @@ def vis_side(df_input_unused=None):
             div[data-testid="stSelectbox"] label { display: none; }
             
             /* Aggressiv reducering af afstand mellem tabs og indhold */
-            .stTabs { margin-top: -10px; }
+            .stTabs { margin-top: -5px; }
             div[data-baseweb="tab-panel"] {
                 padding-top: 0px !important;
-                margin-top: 1px !important;
+                margin-top: 5px !important;
             }
             
             /* Fjern ekstra luft under knapperne til højre */
