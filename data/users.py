5aebdfcb118697f7de3a9b79434a24b2ff90d580
kamudinho <kasper@kaspermundt.dk>
2026-04-09 11:48:06 +0200

    Update users.py

--- data/users.py
+++ data/users.py
@@ -3,39 +3,35 @@ def get_users():
         "kasper": {
             "pass": "kasper1234", 
             "role": "admin",
-            "restricted": []  # Admin ser alt, inklusive EMNELISTE
+            "restricted": []  # Admin ser ALT
         },
         "ceo": {
             "pass": "ceo1234", 
             "role": "Analytiker",
-            "restricted": ["ADMIN"] # Kan se alt undtagen ADMIN, herunder EMNELISTE
+            "restricted": ["ADMIN"] # Alt undtagen admin-panelet
         },
         "mr": {
             "pass": "Retov2650", 
             "role": "manager",
-            "restricted": ["ADMIN"]
+            "restricted": ["ADMIN"] # Alt undtagen admin-panelet
         },
         "kd": {
             "pass": "Daugaard2650", 
             "role": "coach",
-            "restricted": ["ADMIN", "Opret emne", "Emnedatabase"] # Spærret for Emneliste
+            "restricted": ["ADMIN", "Opret emne", "Emnedatabase"] # Ingen adgang til emner
         },
         "cg": {
             "pass": "Gron1234", 
             "role": "scout",
-            "restricted": ["ADMIN", "Opret emne", "Emnedatabase"] # Spærret for Emneliste
+            "restricted": ["ADMIN", "TRUPPEN", "HIF ANALYSE", "BETINIA LIGAEN"] 
+            # CG kan se Scouting-menuen (Rapport, Database, Emnedatabase)
         },
         "jeppe": {
             "pass": "Scout1234", 
             "role": "scout",
             "restricted": [
-                "ADMIN", 
-                "TRUPPEN", 
-                "HIF ANALYSE", 
-                "BETINIA LIGAEN", 
-                "Opret emne", 
-                "Emnedatabase", 
-                "Sammenligning",
+                "ADMIN", "TRUPPEN", "HIF ANALYSE", "BETINIA LIGAEN", "Sammenligning", "Opret emne"
             ] 
+            # Jeppe ser kun Scoutrapport, Database og Emnedatabase under Scouting
         }
     }
