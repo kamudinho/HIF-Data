16dc0f9162c9c08e5da8b85675ed3a1a29e400c7
kamudinho <kasper@kaspermundt.dk>
2026-04-16 22:52:51 +0200

    Update HIF-dash.py

--- HIF-dash.py
+++ HIF-dash.py
@@ -124,7 +124,7 @@ with st.sidebar:
     elif hoved_omraade == "SCOUTING":
         sel = option_menu(None, options=filtrer_menu(["Scoutrapport", "Database", "Emnedatabase", "Sammenligning"]))
     elif hoved_omraade == "ADMIN":
-        sel = option_menu(None, options=filtrer_menu(["System Log", "Profil", "Fysisk profil", "Hold: Fysisk profil", "Intern analyse"]))
+        sel = option_menu(None, options=filtrer_menu(["System Log", "Profil", "Fysisk profil", "Hold: Fysisk profil", "Intern analyse", "Top 5: Spillere"]))
 
 # --- 4. DATA LOADING & RENDERING ---
 render_hif_header(f"{hoved_omraade}  |  {sel.upper()}")
