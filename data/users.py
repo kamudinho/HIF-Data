6bbaac5fc66891a08b9d524a32db58b371c1c469
kamudinho <kasper@kaspermundt.dk>
2026-05-11 20:46:38 +0200

    Update users.py

--- data/users.py
+++ data/users.py
@@ -8,29 +8,29 @@ def get_users():
         "ceo": {
             "pass": "ceo1234", 
             "role": "Analytiker",
-            "restricted": ["ADMIN"] # Alt undtagen admin-panelet
+            "restricted": ["TESTSIDE", "ADMIN"] # Alt undtagen admin-panelet
         },
         "mr": {
             "pass": "Retov2650", 
             "role": "manager",
-            "restricted": ["TILPASNING", "ADMIN"] # Alt undtagen admin-panelet
+            "restricted": ["TESTSIDE", "TILPASNING", "ADMIN"] # Alt undtagen admin-panelet
         },
         "kd": {
             "pass": "Daugaard2650", 
             "role": "coach",
-            "restricted": ["TILPASNING", "ADMIN", "Opret emne", "Emnedatabase"] # Ingen adgang til emner
+            "restricted": ["TESTSIDE", "TILPASNING", "ADMIN", "Opret emne", "Emnedatabase"] # Ingen adgang til emner
         },
         "cg": {
             "pass": "Gron1234", 
             "role": "scout",
-            "restricted": ["TILPASNING", "ADMIN", "TRUPPEN", "HIF ANALYSE", "BETINIA LIGAEN"] 
+            "restricted": ["TESTSIDE", "TILPASNING", "ADMIN", "TRUPPEN", "HIF ANALYSE", "BETINIA LIGAEN"] 
             # CG kan se Scouting-menuen (Rapport, Database, Emnedatabase)
         },
         "jeppe": {
             "pass": "Scout1234", 
             "role": "scout",
             "restricted": [
-                "TILPASNING", "ADMIN", "TRUPPEN", "HIF ANALYSE", "BETINIA LIGAEN", "Sammenligning", "Opret emne"
+                "TESTSIDE", "TILPASNING", "ADMIN", "TRUPPEN", "HIF ANALYSE", "BETINIA LIGAEN", "Sammenligning", "Opret emne"
             ] 
             # Jeppe ser kun Scoutrapport, Database og Emnedatabase under Scouting
         }
