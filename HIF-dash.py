0f912f091cb2912bbd9b937d91badf35a00ec781
kamudinho <kasper@kaspermundt.dk>
2026-05-21 22:33:11 +0200

    Update HIF-dash.py

--- HIF-dash.py
+++ HIF-dash.py
@@ -120,7 +120,7 @@ with st.sidebar:
     st.markdown("""
         <style>
             /* Fjern luft i toppen af sidebaren */
-            [data-testid="stSidebarUserContent"] { padding-top: 0.5rem !important; }
+            [data-testid="stSidebarUserContent"] { padding-top: -2.5rem !important; }
             
             /* Juster margin på den vandrette linje */
             .custom-hr {
