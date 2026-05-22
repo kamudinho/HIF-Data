9280edd07ea7ebb24cb915e0ed4b006040517023
kamudinho <kasper@kaspermundt.dk>
2026-05-22 22:59:05 +0200

    Update HIF_head.py

--- HIF_head.py
+++ HIF_head.py
@@ -258,20 +258,21 @@ def vis_side():
                         <td class='stats-value' style='text-align: center; font-size:10px; color:#666; width: 33%; border-bottom: 1px solid #eee; padding-bottom: 4px;'>{opp_name[:3].upper()}</td>
                     </tr>
                     <tr>
-                        <td class='stats-label' style='text-align: left;'>Mål f/i</td>
+                        <td class='stats-label' style='text-align: left;'>Possession.</td>
+                        <td class='stats-value' style='text-align: center;'>{hif_stats['poss']}</td>
+                        <td class='stats-value' style='text-align: center;'>{opp_stats['poss']}</td>
+                    </tr>
+                    <tr>
+                        <td class='stats-label' style='text-align: left;'>Mål for/imod</td>
                         <td class='stats-value' style='text-align: center;'>{hif_stats['gf']}/{hif_stats['ga']}</td>
                         <td class='stats-value' style='text-align: center;'>{opp_stats['gf']}/{opp_stats['ga']}</td>
                     </tr>
                     <tr>
-                        <td class='stats-label' style='text-align: left;'>xG f/i</td>
+                        <td class='stats-label' style='text-align: left;'>xG for/imod</td>
                         <td class='stats-value' style='text-align: center;'>{hif_stats['xgf']}/{hif_stats['xga']}</td>
                         <td class='stats-value' style='text-align: center;'>{opp_stats['xgf']}/{opp_stats['xga']}</td>
                     </tr>
-                    <tr>
-                        <td class='stats-label' style='text-align: left;'>Poss.</td>
-                        <td class='stats-value' style='text-align: center;'>{hif_stats['poss']}</td>
-                        <td class='stats-value' style='text-align: center;'>{opp_stats['poss']}</td>
-                    </tr>
+                    
                 </table>
                 """
                 st.markdown(stats_html, unsafe_allow_html=True)
