822d7323677c2a29241de39cd61e4c50cd3814d2
kamudinho <kasper@kaspermundt.dk>
2026-07-30 14:39:19 +0200

    Rename 'xG model' to 'Model' in menu options

--- HIF-dash.py
+++ HIF-dash.py
@@ -150,7 +150,7 @@ with st.sidebar:
         "SPILLERANALYSE": ["Spillerprofil"],
         "SCOUTING": ["Scoutrapport", "Database", "Emnedatabase", "Transfers"],
         "TILPASNING": ["Spillerdata", "Spiller-score", "Standardsituationer"],
-        "TESTSIDE": ["xG model", "Performance", "Winning Performance", "1. Div-tilpasning", "Sammenligning", "Charts", "Oversigt", "Forecast"],
+        "TESTSIDE": ["Model", "Performance", "Winning Performance", "1. Div-tilpasning", "Sammenligning", "Charts", "Oversigt", "Forecast"],
         "ADMIN": ["System Log", "Profil", "Datakatalog", "Konklusion", "Fysisk profil", "Hold: Fysisk profil", "Intern analyse", "Top 5: Spillere", "Ordbog"]
     }
     
@@ -269,8 +269,8 @@ try:
         elif s == "Performance":
             import tools.analyse.baseline_performance as bp
             bp.vis_side()
-        elif s == "xG model":
-            import tools.ligaen.xG_model as xg
+        elif s == "Model":
+            import tools.ligaen.model as xg
             xg.vis_side()
 
     elif m == "ADMIN":
