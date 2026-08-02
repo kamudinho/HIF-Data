0cb5e57b4d70c17180f73d2315699a6dbe65eb03
kamudinho <kasper@kaspermundt.dk>
2026-08-02 22:05:27 +0200

    Update sequences.py

--- tools/ligaen/sequences.py
+++ tools/ligaen/sequences.py
@@ -324,15 +324,15 @@ def vis_side(dp=None):
             img_away = get_logo_img(info_row['away_uuid'])
 
             if img_home:
-                pitch.inset_image(x=2.2, y=4.7, image=img_home, height=2.8, ax=ax, zorder=6)
+                pitch.inset_image(x=2.2, y=3.5, image=img_home, height=4.5, ax=ax, zorder=6)
 
-            ax.text(3.5, 3.5, "vs.", fontsize=7, fontweight='bold', ha='center', va='center', color='#333333', zorder=6)
+            ax.text(3.7, 3.5, "vs.", fontsize=7, fontweight='bold', ha='center', va='center', color='#333333', zorder=6)
 
             if img_away:
-                pitch.inset_image(x=4.8, y=4.7, image=img_away, height=2.8, ax=ax, zorder=6)
+                pitch.inset_image(x=5.2, y=3.5, image=img_away, height=4.5, ax=ax, zorder=6)
 
             tekst_bund = f"{info_row['dato']} | Stilling: {info_row['stilling_hjemme_ude']} ({maal_minut}. min)"
-            ax.text(6.5, 3.5, tekst_bund, fontsize=8, color='#555555', ha='left', va='center', zorder=6)
+            ax.text(7.0, 3.5, tekst_bund, fontsize=8, color='#555555', ha='left', va='center', zorder=6)
 
             st.pyplot(fig, use_container_width=True)
 
