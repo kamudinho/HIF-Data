1cd9b13e8f7231d8491bed38472ca32989e565de
kamudinho <kasper@kaspermundt.dk>
2026-08-02 19:40:42 +0200

    Simplify competition map and league IDs
    Removed unused competition mappings and league IDs.

--- tools/ligaen/sequences.py
+++ tools/ligaen/sequences.py
@@ -12,8 +12,8 @@ DB = "KLUB_HVIDOVREIF.AXIS"
 SEASONNAME = "2026/2027"
 TEAM_WYID = 7490
 COMPETITION_WYID = (328,)
-COMP_MAP = { 335: "Superliga", 328: "NordicBet Liga", 329: "2. division", 43319: "3. division", 331: "Oddset Pokalen", 1305: "U19 Ligaen" }
-LIGA_IDS = "('2mb332vncy4450vu14paj8844', '335', '328', '329', '43319', '331')"
+COMP_MAP = { 328: "NordicBet Liga" }
+LIGA_IDS = "('2mb332vncy4450vu14paj8844')"
 
 def oversæt_qualifiers(qual_str):
     if not qual_str or pd.isna(qual_str):
