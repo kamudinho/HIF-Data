8f3fe575f57bbc73b60cf19d3ce52939e8540080
kamudinho <kasper@kaspermundt.dk>
2026-08-02 19:44:48 +0200

    Update sequences.py

--- tools/ligaen/sequences.py
+++ tools/ligaen/sequences.py
@@ -7,12 +7,12 @@ from data.data_load import _get_snowflake_conn
 from data.utils.team_mapping import TEAMS, SEASON_LEAGUE_MAPPER
 from data.utils.mapping import OPTA_EVENT_TYPES, OPTA_QUALIFIERS, get_action_label, har_qualifier
 
-# --- KONFIGURATION (HVIDOVRE-APP) ---
+# --- KONFIGURATION (HVIDOVRE-APP - 2026/2027) ---
 DB = "KLUB_HVIDOVREIF.AXIS"
 SEASONNAME = "2026/2027"
 TEAM_WYID = 7490
 COMPETITION_WYID = (328,)
-COMP_MAP = { 328: "NordicBet Liga" }
+COMP_MAP = { 328: "1. Division", 335: "3F Superliga", 329: "2. division", 43319: "3. division", 331: "Oddset Pokalen", 1305: "U19 Ligaen" }
 LIGA_IDS = "('2mb332vncy4450vu14paj8844')"
 
 def oversæt_qualifiers(qual_str):
