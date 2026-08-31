16fd916401bd5de5fb38fddd2b527e7300ef9e9d
kamudinho <kasper@kaspermundt.dk>
2026-08-31 17:55:42 +0200

    Update HIF-dash.py

--- HIF-dash.py
+++ HIF-dash.py
@@ -233,7 +233,7 @@ try:
             edb.vis_side()
         elif s == "Sammenligning":
             import tools.scouting.sammenligning as comp
-            comp.vis_side(dp["players"], None, None, dp["career"], dp["sql_players"], dp["advanced_stats"], dp.get("primaer_positioner"))
+            comp.vis_side(dp["players"], None, dp["wyscout_players"], dp["career"], dp["sql_players"], dp["advanced_stats"], dp.get("primaer_positioner"))
         elif s == "Transfers":
             import tools.scouting.transfer_input as t_input
             t_input.vis_side()
