f534e5df1690fa395370eece8e02737b67d7d28e
kamudinho <kasper@kaspermundt.dk>
2026-09-17 22:09:19 +0200

    Update users.py

--- data/users.py
+++ data/users.py
@@ -30,6 +30,7 @@ def get_users():
             "role": "scout",
             "restricted": [
                 "TESTSIDE", "TILPASNING", "ADMIN", "TRUPPEN", "HIF ANALYSE", "BETINIA LIGAEN", "Sammenligning", "Opret emne"
+                # "ADMIN_SCOUTING" udelades herfra, så de har adgang til den
             ] 
         },
         "mn": {
