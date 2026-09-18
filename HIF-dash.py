c8902e6478c18b981d19a1ee928494dc0458bf1f
kamudinho <kasper@kaspermundt.dk>
2026-09-18 10:41:39 +0200

    Update HIF-dash.py

--- HIF-dash.py
+++ HIF-dash.py
@@ -3,7 +3,6 @@ import sys
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
-from datatime import datetime
 
 # Sikr at vi kan finde vores egne moduler
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
