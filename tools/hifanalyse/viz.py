26041ad6524bf29d87dec24bfcbc9c4b1abc88fa
kamudinho <kasper@kaspermundt.dk>
2026-09-24 23:50:50 +0200

    Update viz.py

--- tools/hifanalyse/viz.py
+++ tools/hifanalyse/viz.py
@@ -19,7 +19,7 @@ from data.sql.kampe import load_league_match_level_data
 
 HIF_NAVN = "Hvidovre"
 HIF_FARVE = TEAM_COLORS.get(HIF_NAVN, {}).get("primary", "#cc0000")
-MODSTANDER_FARVE = "#FF6666"
+MODSTANDER_FARVE = "#FFF000"
 
 VISNING_MAPPING = {
     "skud_vs_xg": ("SHOTS_FOR", "XG_FOR", "Skud vs. xG"),
