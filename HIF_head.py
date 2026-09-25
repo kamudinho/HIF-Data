bff646b7b6f62450f6c3a069b105f04c41476e14
kamudinho <kasper@kaspermundt.dk>
2026-09-25 08:17:02 +0200

    Update HIF_head.py

--- HIF_head.py
+++ HIF_head.py
@@ -15,6 +15,7 @@ from data.utils.team_mapping import (
 from data.data_load import _get_snowflake_conn
 from data.utils.stattype_map import STAT_TYPE_MAP
 from data.sql.teams import hent_hoved_stats, hent_samlet_hold_statistik
+from data/utils/loaders import load_med_hif_fakta
 
 def apply_custom_style():
     st.markdown("""
